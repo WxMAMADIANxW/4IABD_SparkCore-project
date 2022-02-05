706d698a9898063304d1bdae517f308c1aee611d
WxMAMADIANxW <49515789+WxMAMADIANxW@users.noreply.github.com>
2022-02-05 12:56:27 +0100

    creation flatten_json dataframe

--- src/main/scala/init_load_transform.scala
+++ src/main/scala/init_load_transform.scala
@@ -1,9 +1,13 @@
 import org.apache.spark
+import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
+import org.apache.spark.sql.functions.{array, explode}
 import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
 import sun.util.calendar.LocalGregorianCalendar.Date
-
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{Encoder, Encoders}
 import java.util.Calendar
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
 
 
 
@@ -16,8 +20,9 @@ object init_load_transform extends App{
     .master("local[*]")
     .getOrCreate()
   spark.sparkContext.setLogLevel("ERROR")
+  import spark.implicits._
 
- val schema =  new StructType()
+  val schema =  new StructType()
    .add("video_id",StringType,false)
    .add("title",StringType,false)
    .add("publishedAt",StringType,false)
@@ -45,7 +50,31 @@ object init_load_transform extends App{
     .option("multiLine", true)
     .schema(schema)
     .load("/Users/mamadian_djalo/Documents/ESGI/Spark_Core/Projet/4IABD_SparkCore-project/data/raw_data/BR_youtube_trending_data.csv")
+  println("================ Video =====================")
   df_video.na.drop().show()
+
+  println("================ Categorie =====================")
+
+  val df_categorie = spark.read
+    .option("multiLine","true")
+    .json("/Users/mamadian_djalo/Documents/ESGI/Spark_Core/Projet/4IABD_SparkCore-project/data/raw_data/BR_category_id.json")
+
+  println("-----------PrintSchema Original---------------")
+  df_categorie.printSchema()
+
+  val df_categorie_unnested = df_categorie.select(col("kind"),col("etag"), explode(col("items")) as "level1")
+    .withColumn("id", col("level1.id"))
+    .withColumn("title",col("level1.snippet.title"))
+    .withColumn("assignable", col("level1.snippet.assignable"))
+    .drop("level1")
+
+  
+  /*df_categorie_unnested.select(col("kind"),col("etag"))
+    .withColumn("id", explode(col("id")))
+    .withColumn("title",explode(col("title")))
+    .show(false)*/
+
+
 
 
 
@@ -56,4 +85,33 @@ object init_load_transform extends App{
   *  merge les video csv et les categ json avec  join
   *
   * */
+
+
+  /*def flattenJson(path:StringType): DataFrame={
+    val df_categorie = spark.read
+      .option("multiLine","true")
+      .json("/Users/mamadian_djalo/Documents/ESGI/Spark_Core/Projet/4IABD_SparkCore-project/data/raw_data/BR_category_id.json")
+
+    df_categorie.drop("kind","etag")
+  }
+  def createDf (path: StringType): DataFrame= {
+    val df = spark.read.format("csv")
+      .option("header","true")
+      .option("multiLine", true)
+      .schema(schema)
+      .load("/Users/mamadian_djalo/Documents/ESGI/Spark_Core/Projet/4IABD_SparkCore-project/data/raw_data/BR_youtube_trending_data.csv")
+
+    df.na.drop().show()
+
+
+  df_categorie.select(col("kind"),col("etag"), explode(array("items")) as "level1")
+    .withColumn("id", col("level1.id"))
+    .withColumn("title",col("level1.snippet.title"))
+    .withColumn("assignable", col("level1.snippet.assignable"))
+    .drop("level1").show(false)
+
+  }*/
+
+
 }
+
